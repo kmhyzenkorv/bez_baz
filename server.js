fd0677cefbbca8460798343acd2adf38bd626ebc
Роман Хмызенко <roma.xmyzenko@mail.ru>
2025-03-25 15:28:51 +0500

    added jwt token and tg_auth

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 import express from 'express';
-import session from 'express-session';
-import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
 const app = express();
-const PORT = 3000;
+const PORT = 80;
+const secret = "secret";
+
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use(session({
-    secret: 'admin-secret',
-    saveUninitialized: true,
-    cookie: { httpOnly: true }
-}));
-
+const userData = [
+    { role: 'admin', login: 'admin', password: 'admin' },
+    { role: 'staff', login: 'staff', password: 'staff' },
+    { role: 'user', login: 'user', password: 'user' }
+];
 const options = {root: "public" };
 
 app.get('/', (req, res) => {
@@ -22,15 +24,11 @@ app.get('/', (req, res) => {
 
 app.post('/auth', (req, res) => {
     const { login, password } = req.body;
-    console.log(login, password);
-    const authLogin = ["admin"];
-    const authPassword = ["admin"];
-
-    if (authLogin.includes(login) && authPassword.includes(password)) {
-        req.session.loggedIn = true;
-        req.session.uid = crypto.randomBytes(48).toString('hex');
-        console.log(`UID: ${req.session.uid}`);
-        console.log(`Logged In: ${req.session.loggedIn}`);
+    const user = userData.find(u => u.login === login && u.password === password);
+    if (user) {
+        const token = jwt.sign({ role: user.role, login: user.login }, secret, { expiresIn: '1h' })
+        res.cookie('token', token), {httpOnly: true}
+        console.log(token);
         return res.redirect('/protected');
     } else {
         return res.status(401).sendFile("error.html", options);
@@ -38,29 +36,31 @@ app.post('/auth', (req, res) => {
 });
 
 app.get("/protected", (req, res) => {
-    if (!req.session.loggedIn) {
-        return res.status(402).json({ message: "Неавторизованный доступ" });
-    }
-    res.sendFile("protected.html", options);
+    const token = req.cookies.token;
+    if (token) {
+        jwt.verify(token, secret, (err, decodedToken) => {
+            if (err) {
+                return res.status(403).json({ message: "Токен недействителен" });
+            }
+            const {role} = decodedToken;
+            res.send(`<h1>Привет, ${role}.</h1>`);
+            //res.sendFile("protected.html", options);
+        });
+    } else {
+        return res.status(401).sendFile("error.html", options);
+}
 });
 
-
-app.post('/logout', (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ message: "Ошибка при выходе из системы" });
-        }
-        console.log("Logged Out: true");
-        return res.sendFile("index.html", options);
-    });
+app.get("/telegram", (req, res) => {
+    const tg_data = req.query
+    console.log(tg_data);
+    res.status(200).send("Отправлено в Телеграм!");
 });
 
-app.get("/error", (req, res) => {
-    if (!req.session.loggedIn) {
-    res.sendFile("error.html", options);}
-});
-
+//app.post('/logout', (req, res) => {
+ //   res.clearCookie('token');
+  //  return res.redirect('/');
+//});
 
 
 app.listen(PORT, () => {
